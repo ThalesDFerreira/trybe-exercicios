3a47f4fd331de4de541fe994c3d8b17e8c2850ea
ThalesFerreira <thalesferreira190@gmail.com>
2022-03-07 19:41:40 -0300

    fiz até exercicio 8

--- modulo_1/bloco_4/aula_4.1/programas/scripts8.js
+++ modulo_1/bloco_4/aula_4.1/programas/scripts8.js
@@ -2,9 +2,9 @@ const a = 1;
 const b = 3;
 const c = 5;
 
-let isOdd = false;
+let isEven = false;
 
-if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
-  isOdd = true;
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)) {
+  isEven = true;
 };
-console.log(isOdd);
+console.log(isEven);

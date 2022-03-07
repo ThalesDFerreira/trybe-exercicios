db00ee8bb2485f6f945e79ff413367ba039497dd
ThalesFerreira <thalesferreira190@gmail.com>
2022-03-07 15:06:43 -0300

    finalizei exercicios parte I

--- modulo_1/bloco_4/aula_4.1/script.js
+++ modulo_1/bloco_4/aula_4.1/script.js
@@ -1,5 +1,9 @@
 const myName = "Thales"
 const birthCity = "Porto Velho"
 let birthYear = 1990
+birthYear = 2030
+birthCity = Brasilia
 
-console.log (myName, birthCity, birthYear)
+console.log(myName, birthCity, birthYear)
+console.log(birthYear)
+console.log(birthCity)

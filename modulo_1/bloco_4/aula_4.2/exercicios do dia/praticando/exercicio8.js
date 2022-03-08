39b928d67f679c542ce3ab5c3e773df3dc67dffc
ThalesFerreira <thalesferreira190@gmail.com>
2022-03-08 19:38:57 -0300

    exercicio 9 da pratica

--- modulo_1/bloco_4/aula_4.2/exercicios do dia/praticando/exercicio8.js
+++ modulo_1/bloco_4/aula_4.2/exercicios do dia/praticando/exercicio8.js
@@ -1,6 +1,6 @@
 let numeros = [];
 
-for (let index = 0; index <= 25; index += 1) {
+for (let index = 1; index <= 25; index += 1) {
     numeros.push(index);
 }
 
---
+++ modulo_1/bloco_4/aula_4.2/exercicios do dia/praticando/exercicio9.js
@@ -0,0 +1,18 @@
+let numeros = [];
+
+let numerosDivisao = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    numeros.push(index);
+}
+
+// for (let index2 = 0; index2 < numeros.length; index2 += 1) {
+//     numerosDivisao.push(numeros[index2] / 2);
+// }
+
+for (let index2 = 0; index2 < numeros.length; index2 += 1) {
+    console.log(numeros[index2] / 2);
+}
+
+// console.log(numerosDivisao);
+// console.log(numeros);

7e84febc2b5cae4f6493baa8be6e7ae2cdc636cd
ThalesFerreira <thalesferreira190@gmail.com>
2022-03-09 15:45:42 -0300

    texte com outra palavra exercicio 2

--- modulo_1/bloco_4/aula_4.3/exercicios/exercicio2.js
+++ modulo_1/bloco_4/aula_4.3/exercicios/exercicio2.js
@@ -1,7 +1,7 @@
 // 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". 
 // Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'tryber';
+let word = 'thales';
 
 let invertida = word.split('').reverse().join('');  //split = transforma uma string em um arrey; 
                                                     //reverse = ele pega o arrey e inverte os caracteres;

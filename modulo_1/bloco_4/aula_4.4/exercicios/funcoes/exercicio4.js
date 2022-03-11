46dab667e78ea4aaae422ad20c42b1758060a293
ThalesFerreira <thalesferreira190@gmail.com>
2022-03-11 14:00:50 -0300

    ok

---
+++ modulo_1/bloco_4/aula_4.4/exercicios/funcoes/exercicio4.js
@@ -0,0 +1,20 @@
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
+// Valor esperado no retorno da função: Fernanda .
+
+const names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+
+function biggerName(names) {
+    let quantidadeCaracteres = 0;
+    let result = "";
+    for (let index in names) {
+        if (names[index].length > quantidadeCaracteres) {
+            quantidadeCaracteres = names[index].length;
+            result = names[index];
+        }
+    }
+    return result;
+}
+
+console.log(biggerName());

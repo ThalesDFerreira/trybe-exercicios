7186e38db40031bfff8efae91059b8e1530bb2b8
ThalesFerreira <thalesferreira190@gmail.com>
2022-03-14 15:39:48 -0300

    exercicio de fixaxao do dia 5.1

---
+++ modulo_1/bloco_5/aula_5.1/scripts.js
@@ -0,0 +1,13 @@
+// Agora você! Faça o seguinte:
+
+// 1 - Recupere o elemento que contém o título da página e faça algo com ele, 
+// como alterá-lo para o nome do seu filme favorito.
+
+document.getElementById("page-title").innerText = "Vikings";
+
+// 2 - Agora recupere o segundo parágrafo e use sua criatividade 
+// para alterá-lo.
+
+document.getElementById("second-paragraph").innerText = "Modificado";
+
+// 3 - Por fim, recupere o subtítulo e altere-o também.
---
+++ modulo_1/bloco_5/aula_5.1/scripts1.js
@@ -0,0 +1,6 @@
+// 1 -Para praticar, substitua todas as funções que você usou no exercício anterior por 
+// querySelector e/ou querySelectorAll .
+
+// 2 - Além disso, vamos consolidar a manipulação dos elementos HTML , colocando a cor do 
+// Administrador de Tempo da Trybe como na imagem abaixo usando apenas o JavaScript!
+

d822ed69f2a7b9474251237815c480101fe5a562
ThalesFerreira <thalesferreira190@gmail.com>
2022-04-07 19:19:51 -0300

    add exercicio 2 e 3 do dia 8.1 finalizado

--- modulo_1/bloco_8/aula_8.1/exercicos/script1.js
+++ modulo_1/bloco_8/aula_8.1/exercicos/script1.js
@@ -1,8 +1,12 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
 
 const employeeGenerator = (name) => {
-  const email = name.toLowerCase().split(' ').join('_');
-  return { name, email: `${email}@trybe.com` };
+  // const email = name.toLowerCase().split(' ').join('_');
+  const email = name.toLowerCase().replace(' ', '_');
+  return { 
+    name: name,
+    email: `${email}@trybe.com`,
+  }
 };
 
 const newEmployees = (func) => {
--- modulo_1/bloco_8/aula_8.1/exercicos/script2.js
+++ modulo_1/bloco_8/aula_8.1/exercicos/script2.js
@@ -1,1 +1,17 @@
 // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const resultadoSorteio = (number, callback) => {
+  if (number === callback()) {
+    return 'Parabéns você ganhou';
+  } else {
+    return 'Tente novamente';
+  }
+};
+
+const numeroAleatorio = () => {
+  // const numeroGerado = Math.ceil(Math.random() * 5);
+  const numeroGerado = Math.floor((Math.random() * 5) + 1);
+  return numeroGerado;
+};
+
+console.log(resultadoSorteio(1, numeroAleatorio));

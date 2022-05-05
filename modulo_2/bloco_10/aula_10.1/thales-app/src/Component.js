96164313e4c8ebdea6eba9359e20e88a081182f8
ThalesFerreira <thalesferreira190@gmail.com>
2022-05-05 19:23:29 -0300

    exercicios aula 10.1

---
+++ modulo_2/bloco_10/aula_10.1/thales-app/src/Component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Thales Ferreira</h1>
+        <p>
+          Legal.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Component;
